bd777fe126ab07d2d5c112c3f325eda2592c8824
Luca <ll698@cornell.edu>
2017-11-02 21:06:31 -0400

    loss function still not completed, need to figure out broadcasting between categorial loss from CNN to loss of Resnet

--- resnet.py
+++ resnet.py
@@ -1,33 +1,89 @@
 from keras.layers import Input, Dense, Conv2D, MaxPooling2D, UpSampling2D
 from keras.layers import Dense, Dropout, Flatten, Reshape, Add
-from keras.models import Model
+from keras.models import Model, Sequential
 from keras import backend as K
 from keras.callbacks import TensorBoard
 import matplotlib.pyplot as plt
+import keras
 
 from keras.datasets import mnist
 import numpy as np
 
-(x_train, _), (x_test, _) = mnist.load_data()
+batch_size = 128
+num_classes = 10
+epochs = 1
 
-x_train = x_train.astype('float32') / 255.
-x_test = x_test.astype('float32') / 255.
-x_train = np.reshape(x_train, (len(x_train), 28, 28, 1))  # adapt this if using `channels_first` image data format
-x_test = np.reshape(x_test, (len(x_test), 28, 28, 1))  # adapt this if using `channels_first` image data format
+# input image dimensions
+img_rows, img_cols = 28, 28
+
+# the data, shuffled and split between train and test sets
+(x_train, y_train), (x_test, y_test) = mnist.load_data()
+
+if K.image_data_format() == 'channels_first':
+    x_train = x_train.reshape(x_train.shape[0], 1, img_rows, img_cols)
+    x_test = x_test.reshape(x_test.shape[0], 1, img_rows, img_cols)
+    input_shape = (1, img_rows, img_cols)
+else:
+    x_train = x_train.reshape(x_train.shape[0], img_rows, img_cols, 1)
+    x_test = x_test.reshape(x_test.shape[0], img_rows, img_cols, 1)
+    input_shape = (img_rows, img_cols, 1)
+
+x_train = x_train.astype('float32')
+x_test = x_test.astype('float32')
+x_train /= 255
+x_test /= 255
+print('x_train shape:', x_train.shape)
+print(x_train.shape[0], 'train samples')
+print(x_test.shape[0], 'test samples')
+
+# convert class vectors to binary class matrices
+y_train = keras.utils.to_categorical(y_train, num_classes)
+y_test = keras.utils.to_categorical(y_test, num_classes)
+
+model = Sequential()
+model.add(Conv2D(32, kernel_size=(3, 3),
+                 activation='relu',
+                 input_shape=input_shape))
+model.add(Conv2D(64, (3, 3), activation='relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Dropout(0.25))
+model.add(Flatten())
+model.add(Dense(128, activation='relu'))
+model.add(Dropout(0.5))
+model.add(Dense(num_classes, activation='softmax'))
+
+model.compile(loss=keras.losses.categorical_crossentropy,
+              optimizer=keras.optimizers.Adadelta(),
+              metrics=['accuracy'])
+
+model.fit(x_train, y_train,
+          batch_size=batch_size,
+          epochs=epochs,
+          verbose=1,
+          validation_data=(x_test, y_test))
+score = model.evaluate(x_test, y_test, verbose=0)
+print('Test loss:', score[0])
+print('Test accuracy:', score[1])
+
+#RESNET ------------------------------------------------------------------------------
+
+def res_loss_function(y_true, y_pred, a=0.0, b=1.0):
+    y_true = Reshape((28,28,1))(y_true)
+    baseline = model(y_pred)
+    adverse = model(y_true)
+    classifier_crossentropy = keras.losses.categorical_crossentropy(baseline, adverse)
+    generative_crossentropy = keras.losses.binary_crossentropy(y_true, y_pred)
+    print(generative_crossentropy)
+    l2_distance = K.mean(K.sqrt(2. - (2 * K.sum((y_true * y_pred), axis=-1))))
+
+    out = ((classifier_crossentropy)) + generative_crossentropy
+    print(out)
+    return out
+
+
+
 
 input_img = Input(shape=(28, 28, 1))  # adapt this if using `channels_first` image data format
-
-# This returns a tensor
-inputs = Input(shape=(784,))
-
-# a layer instance is callable on a tensor, and returns a tensor
-x = Dense(64, activation='relu')(inputs)
-x = Dense(64, activation='relu')(x)
-predictions = Dense(10, activation='softmax')(x)
-
-# This creates a model that includes
-# the Input layer and three Dense layers
-
 
 x = Conv2D(32, (3, 3), activation='relu', padding='same')(input_img)
 x = MaxPooling2D((2, 2), padding='same')(x)
@@ -36,27 +92,27 @@ x = MaxPooling2D((2, 2), padding='same')(x)
 x = Conv2D(8, (3, 3), activation='relu', padding='same')(x)
 encoded = MaxPooling2D((2, 2), padding='same')(x)
 
-# 
-# flattened = Flatten()(encoded)
-# dense1 = Dense(128, activation='relu')(flattened)
-# dense2 = Dense(128, activation='relu')(dense1)
-# res_layer = Add()([dense2, flattened])
-# dense3 = Dense(128, activation='relu')(res_layer)
-# dense4 = Dense(128, activation='relu')(dense3)
-# res_layer1 = Add()([dense4, res_layer])
-# #reshaped = Reshape((4,4,8))(res_layer1)
-#
-# dense1 = Dense(128, activation='relu')(res_layer1)
-# dense2 = Dense(128, activation='relu')(dense1)
-# res_layer = Add()([dense2, res_layer1])
-# dense3 = Dense(128, activation='relu')(res_layer)
-# dense4 = Dense(128, activation='relu')(dense3)
-# res_layer1 = Add()([dense4, res_layer])
-# reshaped = Reshape((4,4,8))(res_layer1)
+
+flattened = Flatten()(encoded)
+dense1 = Dense(128, activation='relu')(flattened)
+dense2 = Dense(128, activation='relu')(dense1)
+res_layer = Add()([dense2, flattened])
+dense3 = Dense(128, activation='relu')(res_layer)
+dense4 = Dense(128, activation='relu')(dense3)
+res_layer1 = Add()([dense4, res_layer])
+reshaped = Reshape((4,4,8))(res_layer1)
+
+dense1 = Dense(128, activation='relu')(res_layer1)
+dense2 = Dense(128, activation='relu')(dense1)
+res_layer = Add()([dense2, res_layer1])
+dense3 = Dense(128, activation='relu')(res_layer)
+dense4 = Dense(128, activation='relu')(dense3)
+res_layer1 = Add()([dense4, res_layer])
+reshaped = Reshape((4,4,8))(res_layer1)
 
 # at this point the representation is (4, 4, 8) i.e. 128-dimensional
 
-x = Conv2D(8, (3, 3), activation='relu', padding='same')(encoded)
+x = Conv2D(8, (3, 3), activation='relu', padding='same')(reshaped)
 x = UpSampling2D((2, 2))(x)
 x = Conv2D(16, (3, 3), activation='relu', padding='same')(x)
 x = UpSampling2D((2, 2))(x)
@@ -69,12 +125,21 @@ autoencoder.compile(optimizer='adadelta', loss='binary_crossentropy')
 
 
 autoencoder.fit(x_train, x_train,
-                epochs=4,
+                epochs=1,
                 batch_size=128,
                 shuffle=True,
                 validation_data=(x_test, x_test),
                 callbacks=[TensorBoard(log_dir='/tmp/autoencoder')])
 
+
+autoencoder.compile(optimizer='adadelta', loss=res_loss_function)
+
+autoencoder.fit(x_train, x_train,
+                epochs=50,
+                batch_size=128,
+                shuffle=True,
+                validation_data=(x_test, x_test),
+                callbacks=[TensorBoard(log_dir='/tmp/autoencoder')])
 
 decoded_imgs = autoencoder.predict(x_test)
 
